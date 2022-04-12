2cbaa909d0b1d35a1b3565ed2e6d6cdc0802ffa6
Theonlyhamstertoh <theonlyhamstertoh@gmail.com>
2022-04-12 00:16:31 -0700

    added floating noise ball

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import { useState } from "react";
-
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useState } from "react";
+import randomColor from "randomcolor";
+import { CameraShake, Html, OrbitControls, Scroll, ScrollControls } from "@react-three/drei";
+import { MorphingBall } from "./MorphingBall";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Apple</div>;
+  return (
+    <Canvas>
+      <OrbitControls />
+
+      <fog attach="fog" args={["#7121cc", 0, 50]} />
+      <color attach="background" args={[0x000000]} />
+      <ambientLight />
+      <Suspense>
+        <MorphingBall />
+      </Suspense>
+      <CameraShake yawFrequency={0.2} pitchFrequency={0.2} rollFrequency={0.2} />
+    </Canvas>
+  );
+}
+
+function Box() {
+  const color = randomColor();
+  const randomXYPosition = [
+    Math.floor((Math.random() - 0.5) * 15),
+    Math.floor((Math.random() - 0.5) * 15),
+    0,
+  ];
+  return (
+    <mesh position={randomXYPosition}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color={color} />
+    </mesh>
+  );
 }
 
 export default App;
