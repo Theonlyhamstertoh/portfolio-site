7cc59c323c9a933adb03895be091e1fa541381db
Theonlyhamstertoh <theonlyhamstertoh@gmail.com>
2022-04-12 23:20:21 -0700

    added distortion and speed change when hovering to add to immersion

--- src/MorphingBall.jsx
+++ src/MorphingBall.jsx
@@ -22,10 +22,10 @@ export function MorphingBall() {
   const { scale, distort, speed, wireframe } = useSpring({
     // wireframe: hovered ? true : false,
     scale: hovered ? 1.15 : 1,
-    distort: hovered ? 0.7 : 0.5,
+    distort: hovered ? 0.35 : 0.5,
     speed: hovered ? 5 : 3,
     wireframe: hovered ? false : true,
-    config: { mass: 5, tension: 400, friction: 10 },
+    config: { mass: 3, tension: 300, friction: 10 },
   });
 
   const { lightColor, darkerColor } = useSpring({
@@ -34,10 +34,6 @@ export function MorphingBall() {
     config: config.slow,
   });
 
-  useEffect(() => {
-    // console.log((noiseBall.current.speed = 8));
-    // console.log((noiseBall.current.material.distort = 0.7));
-  });
   useFrame(({ clock }) => {
     const t = clock.getElapsedTime();
     const noiseBallMaterial = noiseBall.current.material;
@@ -50,19 +46,15 @@ export function MorphingBall() {
     noisePoints.current.rotation.z = noiseBall.current.rotation.z;
   });
 
-  const uniforms = useMemo(
-    () => ({
-      uTime: {
-        value: 0,
-      },
-    }),
-    []
-  );
+  /*
+   * Music Track
+   *
+   */
 
   return (
     <group>
       <points ref={noisePoints} receiveShadow>
-        <octahedronBufferGeometry args={[4.3, 50]} />
+        <octahedronBufferGeometry args={[4.4, 50]} />
 
         <a.pointsMaterial size={0.03} color="#000000" />
       </points>
@@ -76,9 +68,9 @@ export function MorphingBall() {
         <icosahedronBufferGeometry args={[4, 30]} />
         <Animated_MeshDistortMaterial
           wireframe={wireframe}
-          speed={3}
+          speed={speed}
           color={lightColor}
-          distort={0.5}
+          distort={distort}
           metalness={0.3}></Animated_MeshDistortMaterial>
       </a.mesh>
     </group>
